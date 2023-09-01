9fde634f42d1f93e9732b6b7a8560af5ee1857e9
Sebastian Lopez Buritica <selobu@gmail.com>
2023-09-01 05:53:25 -0500

    refactor: :recycle: imported pinnia as a plugin

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from './App.vue';
 
 import {vuetify} from './plugins/vuetify.js';
 import router from './router';
+import {pinia} from './plugins/pinia.js';
 
-createApp(App).use(vuetify).use(router).mount('#app')
+createApp(App).use(vuetify).use(router).use(pinia).mount('#app')
