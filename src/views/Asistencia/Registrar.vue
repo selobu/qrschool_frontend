8a222b5459ab4def690a7ba6e5c079f31b99f1c3
Sebastian Lopez Buritica <selobu@gmail.com>
2023-09-05 14:58:01 -0500

    fix: :bug: attempt to fixed height

--- src/views/Asistencia/Registrar.vue
+++ src/views/Asistencia/Registrar.vue
@@ -2,7 +2,7 @@
     <v-container fluid class="px-0 py-0">
         <v-row>
             <v-col cols="12" lg="6">
-                <v-card v-if="!escanear" :height="escanear ? '500px': '150px'" elevation="3" class="px-0">
+                <v-card v-if="!escanear" :max-height="escanear ? '500px': '150px'" elevation="3" class="px-0">
                     <v-card-title :class="$vuetify.theme.name === 'dark' ? 'bg-grey-darken-3 py-0' : 'py-0 bg-info'"> Registrar códigos </v-card-title>
                     <br/>
                     <v-card-actions>
@@ -11,7 +11,7 @@
                         <v-spacer></v-spacer>
                     </v-card-actions>
                 </v-card>
-                <qr-reader v-else :multiple="true" v-model="userslist" height="500">
+                <qr-reader v-else :multiple="true" v-model="userslist" max-height="500px">
                     <template v-slot:actions>
                         <v-btn variant="outlined" color="error" @click="escanear=false">
                             Cancelar
@@ -24,7 +24,7 @@
                 </qr-reader>
             </v-col>
             <v-col cols="12" lg="6">
-                <v-card height="500" elevation="3">
+                <v-card max-height="500px" elevation="3">
                     <v-card-title :class="$vuetify.theme.name === 'dark' ? 'bg-grey-darken-3 py-0' : 'py-0 bg-warning'">Listado</v-card-title>
                     <v-card-text>
                         <p><br/><strong>Total registrados: 8</strong></p>
