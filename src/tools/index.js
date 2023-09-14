32e8270386f600dc77ec57e6cf2a81e2cda14df4
Sebastian Lopez Buritica <selobu@gmail.com>
2023-09-14 11:01:15 -0500

    refactor: :recycle: organized urls and simplify time function

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,14 +1,10 @@
-var Url = "";
-
-if (import.meta.env.PROD) {
-  Url = import.meta.env.VITE_APP_apiURL;
-} else {
-  Url = import.meta.env.VITE_APP_apiLocal;
-}
 import md5 from 'md5'
 
-const apiUrl = Url;
-export const url = Url;
+export var url = import.meta.env.PROD ? import.meta.env.VITE_APP_apiURL : import.meta.env.VITE_APP_apiLocal
+export var bApiUrl = import.meta.env.PROD ? import.meta.env.VITE_APP_apiBaseURL : import.meta.env.VITE_APP_apiBaseURL
+
+url = url.slice(-1) === '/' ? url : `${url}/` 
+bApiUrl = bApiUrl.slice(-1) === '/' ? bApiUrl : `${bApiUrl}/`
 
 export function isadmin() {
   try {
@@ -17,47 +13,29 @@ export function isadmin() {
     return false;
   }
 }
+
 export function today() {
   return now().split(" ")[0];
 }
-export function firstday() {
-  return now().split(" ")[0].substr(0, 8) + "01";
-}
-export function now() {
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1; //January is 0!
-  var yyyy = today.getFullYear();
-  var hours = today.getHours();
-  var minutes = today.getMinutes();
-  var seconds = today.getSeconds();
 
-  mm = mm < 10 ? "0" + mm : mm;
-  dd = dd < 10 ? "0" + dd : dd;
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-  today =
-    "" +
-    yyyy +
-    "-" +
-    mm +
-    "-" +
-    dd +
-    " " +
-    hours +
-    ":" +
-    minutes +
-    ":" +
-    seconds;
-  return today;
+export function firstday() {
+  return now().split(" ")[0].slice(0, 8) + "01";
 }
-export function getGravatar(email, size = 80) {
-  if (email === null){
-    email=''
-  } 
-  return `https://www.gravatar.com/avatar/${md5(email)}?s=${size}`;
+
+export function now() {
+  const fix = (time) => time < 10 ? `0${time}` : `${time}`
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const dd = fix(today.getDate());
+  const mm = fix(today.getMonth() + 1); //January is 0!
+  const hours = fix(today.getHours());
+  const minutes = fix(today.getMinutes());
+  const seconds = fix(today.getSeconds());
+  
+  return `${yyyy}-${mm}-${dd} ${hours}:${minutes}:${seconds}`
 }
+
+export const getGravatar = (email, size=80) => `https://www.gravatar.com/avatar/${md5(email || '')}?s=${size}`
 
 export function mask(cc, num = 6, mask = '*'){
   return `${cc}`.slice(0,-num).padEnd(`${cc}`.length, mask);
