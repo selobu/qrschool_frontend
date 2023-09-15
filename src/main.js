2feff31aad162bea3a5e6aac13c6918b9355d5a2
Sebastian Lopez Buritica <selobu@gmail.com>
2023-09-15 10:14:47 -0500

    feat: :heavy_minus_sign: formkit removed from mainjs file

--- src/main.js
+++ src/main.js
@@ -5,13 +5,9 @@ import App from './App.vue';
 import {vuetify} from './plugins/vuetify.js';
 import router from './router';
 import {pinia} from './plugins/pinia.js';
-import { plugin } from '@formkit/vue'
-import { config } from './plugins/formkit.js'
-
 
 createApp(App).
     use(vuetify).
     use(router).
     use(pinia).
-    use(plugin, config).
     mount('#app')
