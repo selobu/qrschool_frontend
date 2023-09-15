9232abc785a9044aa73c1273890807eca0b37789
Sebastian Lopez Buritica <selobu@gmail.com>
2023-09-15 07:28:35 -0500

    feat: :heavy_plus_sign: maska dependency added

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ import App from './App.vue';
 import {vuetify} from './plugins/vuetify.js';
 import router from './router';
 import {pinia} from './plugins/pinia.js';
+import { vMaska } from "maska"
 
-createApp(App).use(vuetify).use(router).use(pinia).mount('#app')
+createApp(App).use(vuetify).use(router).use(pinia).
+    directive("maska", vMaska).mount('#app')
