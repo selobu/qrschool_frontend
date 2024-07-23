d2508e9281a2454d6e8c0c7f348fb596077b8df2
influurSebastian <sebastian.lopez@influur.com>
2024-07-23 17:49:12 -0500

    fix: :bug: Fix auth store

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -87,7 +87,7 @@
     },
     methods:{
       async authuser(access_token, auth, email, fresh_access_token, username, qr, active, modules){
-        this.data.authstore.login(access_token, auth, email, fresh_access_token, username, qr, active, modules).then(
+        authStore().login(access_token, auth, email, fresh_access_token, username, qr, active, modules).then(
          this.$router.push({name: 'mostrarmiqr'})
         )
       },
@@ -97,7 +97,7 @@
           const results = await event
           if (!results.valid) return
           const response = await post('login/', {email:this.email, password:this.password}, false)
-          if (response.status !== 200){
+          if (response?.status !== 200){
             return
           }
           const {access_token, auth, email, fresh_access_token, username, qr, active, modules} = response.data
